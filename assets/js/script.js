1e6895db1855764bd1a1a275d2f32f2ee8ab3c25
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-06-20 12:19:58 +0430

    ui to the project added

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// INSPIRED BY SEB KAY'S 'SIGN UP FORM'
+// https://dribbble.com/shots/1776663-Purple-Sign-Up-Form
+// https://dribbble.com/shots/1779149--Free-PSD-Purple-Sign-Up-Form-Payment-Details
+// @purple: #c68eaa;
+// @green: #f0a500;
+// #64ac15  bd8200
+
+// #7ed321 light f0a500
